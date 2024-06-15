3e0aaadf287a21578c7a21fc28bc892ef77b945c
ricardo <ridaoc19@gmail.com>
2024-06-15 11:58:02 +0200

    feat(userFilterData): cration custom hooks useFilterData

---
+++ src/hooks/useFilterData.tsx
@@ -0,0 +1,23 @@
+import { useContext, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { CreateContext } from './useContext';
+import { MarvelState } from './useContext/reducer';
+
+function useFilterData(): MarvelState & { totalFavorites: number } {
+	const { marvelState, requestCharacters, requestComic } =
+		useContext(CreateContext);
+	const { characterId } = useParams<{ characterId: string }>();
+
+	useEffect(() => {
+		if (marvelState.character.length === 0) requestCharacters();
+		if (characterId) requestComic(characterId);
+		// eslint-disable-next-line
+	}, [characterId]);
+
+	return {
+		...marvelState,
+		totalFavorites: marvelState.character.filter(e => e.favorite).length,
+	};
+}
+
+export default useFilterData;
