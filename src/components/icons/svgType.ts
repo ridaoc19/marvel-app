4b983461de044ce8e473387785cd18b9b17a804d
ricardo <ridaoc19@gmail.com>
2024-06-15 10:50:03 +0200

    feat(button-icons): creation global compoents button and icons

---
+++ src/components/icons/svgType.ts
@@ -0,0 +1,14 @@
+export enum SvgType {
+	Filter = 'filter',
+	Out = 'out',
+	Logo = 'logo',
+	Search = 'Search',
+	Spanish = 'Spanish',
+	English = 'English',
+	Favorites = 'Favorites',
+	Favorites_red = 'Favorites_red',
+	Favorites_white = 'Favorites_white',
+	Favorites_black = 'Favorites_black',
+	Favorites_black_size = 'Favorites_black_size',
+	Favorites_white_size = 'Favorites_white_size',
+}
